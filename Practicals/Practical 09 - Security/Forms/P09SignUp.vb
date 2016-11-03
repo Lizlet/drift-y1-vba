8699f1b94cf52c4dd3ec414c6bced84cabeb8bd4
Sondre Groneng <songro93@gmail.com>
2016-11-03 13:19:09 +0100

    red color when password is too short, or repeat password is shorter than password

--- Practicals/Practical 09 - Security/Forms/P09SignUp.vb
+++ Practicals/Practical 09 - Security/Forms/P09SignUp.vb
@@ -9,7 +9,11 @@
         End Sub
 
         Public Sub Submitted() Implements P09Form.Submitted
-            If TextBox2.Text = TextBox3.Text Then
+            If TextBox1.TextLength <= 4 Then
+                MsgBox("username too short")
+            ElseIf TextBox2.TextLength <= 4 Then
+                MsgBox("Password too short")
+            ElseIf TextBox2.Text = TextBox3.Text Then
                 owner.addNewUser(TextBox1.Text, TextBox2.Text)
                 owner.changeForm(New P09Default(owner))
             Else
@@ -19,6 +23,22 @@
 
         Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
             owner.changeForm(New P09Default(owner))
+        End Sub
+
+        Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+            If TextBox2.TextLength <= 4 Then
+                TextBox2.BackColor = Color.Red
+            Else
+                TextBox2.BackColor = Color.White
+            End If
+        End Sub
+
+        Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+            If TextBox3.TextLength <= 4 Or TextBox3.TextLength < TextBox2.TextLength Then
+                TextBox3.BackColor = Color.Red
+            Else
+                TextBox3.BackColor = Color.White
+            End If
         End Sub
     End Class
 End Namespace
